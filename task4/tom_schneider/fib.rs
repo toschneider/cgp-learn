76234e4b43514670e0689ddaadb50ccb5e8f85c3
toschneider <toschneider@cip80.informatik.uni-osnabrueck.de>
2016-07-20 11:54:36 +0200

    comment

--- task4/tom_schneider/fib.rs
+++ task4/tom_schneider/fib.rs
@@ -1,28 +1,26 @@
-struct Typ {
+struct Fib {
     prev: u32,
     cur: u32,
 }
 
-impl Iterator for Typ {
+impl Iterator for Fib {
     type Item = u32;
     fn next(&mut self) -> Option<u32> {
-        // if  {
+
         let tmp = self.prev + self.cur;
         self.prev = self.cur;
         self.cur = tmp;
         Some(self.prev)
-        // } else {
-        //     None
-        // }
+
     }
 }
-impl Typ {
-    fn new() -> Typ {
-        Typ { cur: 1, prev: 0 }
+impl Fib {
+    fn new() -> Fib {
+        Fib { cur: 1, prev: 0 }
     }
 }
 fn main() {
-    for i in Typ::new().take(20) {
+    for i in Fib::new().take(20) {
         println!("{}", i);
     }
 }
